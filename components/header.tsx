21d2a5c7599d75058cf83639649745c4c595a06d
Leonel Sanches <leonel.sanches@segment.com>
2022-09-22 15:03:45 -0700

    Separating header into another component.

---
+++ components/header.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Image from 'next/image'
+
+export const Header = () => (
+    <header>
+      <div className='container my-5'>
+        <div className='row'>
+          <div className='col f-left'>
+            <Image className="m-auto" src="/logo.png" alt="Leo's Guitar Shop" width={502} height={55} />
+          </div>
+          <div className='col f-right'>
+            <div className="input-group mb-3">
+              <input type="text" className="form-control" placeholder="Search the store" aria-label="Search the store" aria-describedby="button-addon2" />
+              <button className="btn btn-dark" type="button" id="search-button">
+                <i className="bi bi-search"> </i>
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+)
---
+++ components/index.ts
@@ -0,0 +1,2 @@
+export * from './guitar-card'
+export * from './header'
