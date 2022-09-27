0fac38f5fc565c22e4dbfa550a3d89be8b6e7f7e
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2022-09-27 09:10:23 -0700

    Subscription flow.

--- components/newsletter/subscription-form.tsx
+++ components/newsletter/subscription-form.tsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Image from 'next/image'
 import Link from 'next/link'
 
-export const SubscriptionForm = () => (
-    <div className="input-group mb-3">
-        <input 
-            type="email" 
-            id="subscription-email" 
-            name="subscription-email" 
-            className="form-control" 
-            placeholder="Your email address" 
-            aria-label="Your email address" 
-            aria-describedby="subscribe-button" />
+export const SubscriptionForm = () => {
+    const [email, setEmail] = useState('')
+    
+    const subscribeToNewsletter: Function = () => {
+        alert(`You are now subscribed to our newsletter.`)
+    }
+
+    return <div className="input-group mb-3">
+        <input
+            type="email"
+            id="subscription-email"
+            name="subscription-email"
+            className="form-control"
+            placeholder="Your email address"
+            aria-label="Your email address"
+            aria-describedby="subscribe-button" 
+            onChange={(event) => setEmail(event.target.value)}/>
         <button className="btn btn-dark" type="button" id="subscribe-button">
-            <i className="bi bi-newspaper"> </i>
+            <i className="bi bi-newspaper" onClick={() => subscribeToNewsletter()}> </i>
             Subscribe
         </button>
     </div>
-)
+}
