dd5054b148540c060944578865348927257ad019
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2022-10-17 12:25:38 -0700

    Fixing typo.

--- data/mocks/guitars.ts
+++ data/mocks/guitars.ts
@@ -1,7 +1,7 @@
 import { IGuitar } from "../../models";
 
 export default [
-    { id: 1, model: "Gibson Semi Acostic with tremolo", manufacturer: "Gibson", price: 650, mainImage: "https://cdn.pixabay.com/photo/2016/10/12/23/22/electric-guitar-1736291_960_720.jpg" },
+    { id: 1, model: "Gibson Semi Acoustic with tremolo", manufacturer: "Gibson", price: 650, mainImage: "https://cdn.pixabay.com/photo/2016/10/12/23/22/electric-guitar-1736291_960_720.jpg" },
     { id: 2, model: "Ibanez Prestige", manufacturer: "Ibanez", price: 1590, mainImage: "https://cdn.pixabay.com/photo/2017/04/09/16/11/guitar-2216068_960_720.jpg" },
     { id: 3, model: "Fender Player Stratocaster Sunburst", manufacturer: "Fender", price: 850, mainImage: "https://cdn.pixabay.com/photo/2017/11/07/00/18/guitar-2925274_960_720.jpg" },
     { id: 4, model: "Guitar Toy", manufacturer: "Hasbro", price: 50 },
