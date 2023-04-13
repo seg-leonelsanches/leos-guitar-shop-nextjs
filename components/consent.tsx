6d7d4260e770e2796cafd7fe661acb08b0b09a9c
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2023-04-13 09:41:32 -0700

    Adjusting Consent Manager banner to always show, for demo purposes.

--- components/consent.tsx
+++ components/consent.tsx
@@ -21,17 +21,11 @@ export const Consent = (props: any) => {
 
   return (
     <>
-      <Script
-        id="show-banner"
-        dangerouslySetInnerHTML={{
-          __html: `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};;analytics.SNIPPET_VERSION="4.13.2";
-          analytics.page();
-          }}();`
-        }}
-      />
       <ConsentManager
         writeKey={props.writeKey}
-        shouldRequireConsent={inEU}
+        shouldRequireConsent={() => true}
+        // Uncomment below to configure the component to only show the banner in the EU
+        // shouldRequireConsent={inEU}
         bannerContent={bannerContent}
         bannerSubContent={bannerSubContent}
         preferencesDialogTitle={preferencesDialogTitle}
